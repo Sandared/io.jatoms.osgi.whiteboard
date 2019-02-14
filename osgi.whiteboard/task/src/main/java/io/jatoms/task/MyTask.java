572fdc1c49f844bb9d85c70f60d2b6a7ece9cac7
Sandared <thomas.driessen.td@gmail.com>
2019-02-14 14:42:08 +0000

    minor changes

--- osgi.whiteboard/task/src/main/java/io/jatoms/task/MyTask.java
+++ osgi.whiteboard/task/src/main/java/io/jatoms/task/MyTask.java
@@ -5,7 +5,7 @@ import org.osgi.service.component.annotations.Component;
 import io.jatoms.osgi.whiteboard.api.ITask;
 
 @Component
-public class ComponentImpl implements ITask{
+public class MyTask implements ITask{
 	@Override
 	public void run() {
         System.out.println("Hello Whiteboard!");

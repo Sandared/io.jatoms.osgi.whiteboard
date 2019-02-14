c214e83838f7678a9178593f5eef1150ad420138
Sandared <thomas.driessen.td@gmail.com>
2019-02-14 14:37:26 +0000

    Simplified Whiteboard impl

--- osgi.whiteboard/impl/src/main/java/io/jatoms/osgi/whiteboard/TaskWhiteboard.java
+++ osgi.whiteboard/impl/src/main/java/io/jatoms/osgi/whiteboard/TaskWhiteboard.java
@@ -1,57 +1,37 @@
 package io.jatoms.osgi.whiteboard;
 
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
 
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.component.annotations.ReferenceCardinality;
 import org.osgi.service.component.annotations.ReferencePolicy;
-import org.osgi.service.component.annotations.ReferencePolicyOption;
 
 import io.jatoms.osgi.whiteboard.api.ITask;
 
-@Component
-public class TaskWhiteboard {
-    private List<ITask> tasks = new CopyOnWriteArrayList<>();
-    private Executor exec = Executors.newSingleThreadExecutor();
-    private volatile boolean stopped = false;
+@Component(immediate = true)
+public class TaskWhiteboard extends TimerTask{
+    @Reference(policy=ReferencePolicy.DYNAMIC)
+    private final List<ITask> tasks = new CopyOnWriteArrayList<>();
+
+    private Timer timer = new Timer();
 
     @Activate
     void activate () {
-        exec.execute(() -> {
-            while(!stopped){
-                tasks.forEach((task)->{
-                    try {
-                        task.run();
-                    } catch (Throwable t) {
-                        t.printStackTrace();
-                    }
-                });
-                try {
-					Thread.sleep(1000);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-            }
-        });
+        timer.scheduleAtFixedRate(this, 0, 1000);
     }
 
     @Deactivate
     void deactivate() {
-        stopped = true;
+        timer.cancel();
     }
 
-    @Reference(cardinality=ReferenceCardinality.MULTIPLE, policy=ReferencePolicy.DYNAMIC, policyOption=ReferencePolicyOption.GREEDY)
-    void addTask(ITask task){
-        tasks.add(task);
-    }
-
-    void removeTask(ITask task){
-        tasks.remove(task);
-    }
+	@Override
+	public void run() {
+        tasks.forEach(task -> task.run());
+	}
 }
